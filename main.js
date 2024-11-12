0b9b3193500c53581acf7a942f939f308c53a519
ChaseDevSmith <50593929+ChaseDevSmith@users.noreply.github.com>
2024-11-12 02:51:35 -0600

    init and planet properly referenced in main.js
    hopefully

--- main.js
+++ main.js
@@ -3,8 +3,9 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.128.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
 
-import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.11.0/dist/gsap.min.js';import { initScene } from './init';
-import {Planet} from './planets'
+import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.11.0/dist/gsap.min.js';
+import { initScene } from '/init';
+import {Planet} from '/planets'
 
 ///scene init, camera init, light init
 const {camera, scene, renderer} = initScene()
