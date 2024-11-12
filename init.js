a4b8ca3c73e90cc4442522fc1caa910068b8dfa6
ChaseDevSmith <50593929+ChaseDevSmith@users.noreply.github.com>
2024-11-12 02:28:24 -0600

    properly importing external libraries in init.js

--- init.js
+++ init.js
@@ -1,6 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
+import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 
 export function initScene(){
 const scene = new THREE.Scene();
