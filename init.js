a6b3f8a6f90aec7a87c38cc88d0a69c901543cc0
ChaseDevSmith <50593929+ChaseDevSmith@users.noreply.github.com>
2024-11-12 02:46:41 -0600

    got rid of import of three.js

--- init.js
+++ init.js
@@ -1,4 +1,3 @@
-import * as THREE from 'https://unpkg.com/three@0.128.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 
 export function initScene(){
