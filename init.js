c01570e63b0bc978a95fe598cf2a9e1078a6dbfa
ChaseDevSmith <50593929+ChaseDevSmith@users.noreply.github.com>
2024-11-28 04:07:07 -0600

    canvas
    changed how i want to pass in canvas

--- init.js
+++ init.js
@@ -5,15 +5,19 @@ export function initScene(){
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5.7;
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const canvas = document.querySelector("#canvas");
+  
+  const renderer = new THREE.WebGLRenderer({antialias:true,canvas});
+  renderer.setPixelRatio(window.devicePixelRatio);
+  
+  
+  renderer.setSize( window.innerWidth, window.innerHeight );
 const light = new THREE.DirectionalLight(0x404040, 500);
 const light2 = new THREE.HemisphereLight(0x404040, 0x404040, 7);
 const light4 = new THREE.AmbientLight(0xffffff, 1);
 scene.add( light, light2 ,);
 //orbit controls
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls( camera, canvas );
 controls.update();
   controls.enabled = true
 let controlsEnabled = true;
